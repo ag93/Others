dacfee4e50949bb1f212580971905b7f08a2b1b3
Aniket Gade <34727005+ag93@users.noreply.github.com>
2018-03-08 23:48:45 -0600

    Add files via upload

---
+++ Learning Matplotlib/7a. Plotting from a file using numpy.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
+import numpy as np
+
+x, y = np.loadtxt('test.csv', delimiter=',', unpack=True)
+plt.plot(x,y, label='Loaded from file!')
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Interesting Graph\nCheck it out')
+plt.legend()
+plt.show()

959aa0d073495018c2cf0bdbdddb17d201ee3c7d
Aniket Gade <34727005+ag93@users.noreply.github.com>
2018-03-21 16:11:27 -0500

    Add files via upload

---
+++ Learning Matplotlib/9. Box Plots.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 21 12:17:09 2018
+
+@author: Aniket Gade
+"""
+
+import matplotlib.pyplot as plt
+
+data_1 = [[56,47,49,37,38,60,50,43,43,59,50,56,54,58],[53,21,32,49,45,38,44,33,32,43,53,46,36,48,39,35,37,36,39,49]]
+plt.boxplot(data_1)
+plt.show()

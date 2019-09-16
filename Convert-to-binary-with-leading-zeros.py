3dd40776640a185c9abdc8312f463786f2417c5b
Aniket Gade <34727005+ag93@users.noreply.github.com>
2019-09-15 22:38:06 -0500

    Add files via upload

---
+++ Convert-to-binary-with-leading-zeros.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 12 22:56:18 2019
+
+@author: anike
+"""
+x = 20
+y = 25
+
+
+n1 = '{0:031b}'.format (x)
+n2 = '{0:031b}'.format (y)
+
+print(x, n1)
+print(y, n2)
